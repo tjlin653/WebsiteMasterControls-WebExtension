05bf7c4a41b26a0bd32b1b72aec6597e201dd95d
tjlin653 <tonylin415@gmail.com>
2026-02-08 20:40:49 -0800

    combining changing font size number display and the actual change

--- content.js
+++ content.js
@@ -22,34 +22,27 @@ linkColor.addEventListener('input', function() {
 });
 
 const fontSizeDisplay = document.getElementById('fontSizeDisplay');
-function decreaseFontSize() {
+
+let subtractFontSize = document.getElementById('subtractFontSize');
+subtractFontSize.addEventListener('click', () => {
     let currentFontValue = parseFloat(fontSizeDisplay.textContent);
     if (!isNaN(currentFontValue)) {
         currentFontValue--;
         fontSizeDisplay.textContent = currentFontValue;
     }
-};
-
-let subtractFontSize = document.getElementById('subtractFontSize');
-subtractFontSize.addEventListener('click', () => {
-    decreaseFontSize();
     document.querySelectorAll('h1, h2, h3, h4, h5, h6, p, a, span, li').forEach(textSize => {
         let currentFontSize = parseFloat(window.getComputedStyle(textSize).fontSize);
         textSize.style.fontSize = (currentFontSize -1) + 'px';
     });
 });
 
-function increaseFontSize() {
+let addFontSize = document.getElementById('addFontSize');
+addFontSize.addEventListener('click', () => {
     let currentFontValue = parseFloat(fontSizeDisplay.textContent);
     if (!isNaN(currentFontValue)) {
         currentFontValue++;
         fontSizeDisplay.textContent = currentFontValue;
     }
-};
-
-let addFontSize = document.getElementById('addFontSize');
-addFontSize.addEventListener('click', () => {
-    increaseFontSize();
     document.querySelectorAll('h1, h2, h3, h4, h5, h6, p, a, span, li').forEach(textSize => {
         let currentFontSize = parseFloat(window.getComputedStyle(textSize).fontSize);
         textSize.style.fontSize = (currentFontSize +1) + 'px';
