773a58095f7b034f6864dbd9e4ca1b911be0ba0c
tjlin653 <tonylin415@gmail.com>
2026-02-09 08:07:27 -0800

    changing letter spacing

--- content.js
+++ content.js
@@ -35,7 +35,6 @@ subtractFontSize.addEventListener('click', () => {
         textSize.style.fontSize = (currentFontSize -1) + 'px';
     });
 });
-
 let addFontSize = document.getElementById('addFontSize');
 addFontSize.addEventListener('click', () => {
     let currentFontValue = parseFloat(fontSizeDisplay.textContent);
@@ -63,7 +62,6 @@ subtractLineHeight.addEventListener('click', () => {
         textHeight.style.lineHeight = (currentLineHeight -1) + 'px';
     });
 });
-
 let addLineHeight = document.getElementById('addLineHeight');
 addLineHeight.addEventListener('click', () => {
     let currentLineValue = parseFloat(lineHeightDisplay.textContent);
@@ -74,5 +72,32 @@ addLineHeight.addEventListener('click', () => {
     document.querySelectorAll('h1, h2, h3, h4, h5, h6, p, a, span, li').forEach(textHeight => {
         let currentLineHeight = parseFloat(window.getComputedStyle(textHeight).lineHeight);
         textHeight.style.lineHeight = (currentLineHeight +1) + 'px';
+    });
+});
+
+const letterSpaceDisplay = document.getElementById('letterSpaceDisplay');
+
+subtractLetterSpace = document.getElementById('subtractLetterSpace').addEventListener('click', () => {
+    let currentSpacingValue = parseFloat(letterSpaceDisplay.textContent);
+    if (!isNaN(currentSpacingValue)) {
+        currentSpacingValue--;
+        letterSpaceDisplay.textContent = currentSpacingValue;
+    }
+    document.querySelectorAll('h1, h2, h3, h4, h5, h6, p, a, span, li').forEach(textSpacing => {
+        let computedLetterSpace = window.getComputedStyle(textSpacing).letterSpacing;
+        let currentLetterSpace = computedLetterSpace === "normal" ? 0 : parseFloat(computedLetterSpace);
+        textSpacing.style.letterSpacing = (currentLetterSpace -1) + 'px';
+    });
+});
+addLetterSpace = document.getElementById('addLetterSpace').addEventListener('click', () => {
+    let currentSpacingValue = parseFloat(letterSpaceDisplay.textContent);
+    if (!isNaN(currentSpacingValue)) {
+        currentSpacingValue++;
+        letterSpaceDisplay.textContent = currentSpacingValue;
+    }
+    document.querySelectorAll('h1, h2, h3, h4, h5, h6, p, a, span, li').forEach(textSpacing => {
+        let computedLetterSpace = window.getComputedStyle(textSpacing).letterSpacing;
+        let currentLetterSpace = computedLetterSpace === "normal" ? 0 : parseFloat(computedLetterSpace);
+        textSpacing.style.letterSpacing = (currentLetterSpace +1) + 'px';
     });
 });
